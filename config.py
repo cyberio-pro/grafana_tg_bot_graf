bf0eb0f03c29756117d3d8f21242bf5b12fde6d3
Stanislav <138219377+Stanlyio@users.noreply.github.com>
2023-12-04 09:43:58 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+#example config
+apihelper_proxy = {'https':'socks5://user:passwd@180.20.20.20:1234'}
+
+grafana_token = {'Authorization': 'Bearer eyJrIjoiUHBiaU1LcDNCVTJQRkJzMlZLTzJlSXRlGmt5WkVUa2siKCJuIjoidGVzdDIiLCJpZCI6MX0='}
+
+image_path = '/home/user/PycharmProjects/GrafanaScreen-TlgBot/app/GrafanaScreen-TlgBot/image/'
+
+grafana_url = 'http://192.168.231.6:3000'
+
+bot_token = '000000000:AAGNBmRWGDyIZb-i9evBbZ1N9JJqNoQxjdE'
+
+user_list = [50000000, 276548653]
+
+check = 1
+
+command_list = ['start', 'Stop', 'List', 'Pwd12',
+                'bot-testing-dashboard', 'Carbon agents localdomain', 'Metric recieved',
+                'open-dashboard', 'Commited points', 'Memussage', 'go back']
+
+logfile = '/home/user/PycharmProjects/GrafanaScreen-TlgBot/app/GrafanaScreen-TlgBot/log.txt'
+
+admin_id = 50000000
+
